a01300f8d3da599a17a2ec5577a12c9a9e6958c9
Md Shayemur Rahman <sayemurrahman99@gmail.com>
2022-07-04 03:57:54 +0300

    Show Walking Data and Set Goal

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import User from "./components/User";
+import Add from "./components/Add";
+import Login from "./components/Login";
 
-import "./App.css";
-import Index from "./routes";
-
-const App = () => {
-  return <Index />;
-};
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/Dashboard" component={User} />
+        <Route exact path="/add" component={Add} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
 
 export default App;
